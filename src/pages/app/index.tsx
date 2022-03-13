88bc35ecd6b56380fec4022bf19be9956801107d
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2022-03-12 22:05:03 -0300

    melhoria no layout

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,31 +1,91 @@
-import { Button, Flex, Heading, Stack } from '@chakra-ui/react';
+/* eslint-disable jsx-a11y/alt-text */
+import { Flex, Image, Menu, MenuButton, MenuItem, MenuList, Stack, Text, Link, Box } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
 import { signOut, useSession } from 'next-auth/react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { prisma } from '../../lib/prisma';
 
-const Hello: React.FC = () => {
+export default function Hello({ tasks }) {
+
   const { data } = useSession()
+
+  useEffect(() => {
+    console.log(data)
+  }, [])
+
+
   return (
     <Flex
-      bg='gray.700'
+      bg='gray.200'
       w='100%'
       h='100vh'
-      justifyContent='center'
       alignItems='center'
+      direction='column'
     >
-      <Stack p='4'>
-        <Heading color='white'>Seja bem vindo: {JSON.stringify(data?.user?.name)}</Heading>
-        <Button
-          colorScheme={'purple'}
-          size='lg'
-          onClick={() => signOut()}
-        >
-          Deslogar
-        </Button>
-      </Stack>
+      <Flex
+        w='100%'
+        h='80px'
+        alignItems='center'
+        pl='12'
+        pr='12'
+        bg='gray.700'
+      >
 
+        <Box ml='auto'>
+          <Menu>
+            <MenuButton as={Link} >
+              <Stack
+                direction='row'
+                alignItems='center'
+                color='white'
+                spacing='4'
+              >
+                <Text >Olá, {data?.user.name}</Text>
+                <Image
+                  src={data?.user?.image}
+                  borderRadius='full'
+                  boxSize='60px'
+                />
+              </Stack>
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={() => signOut()}>Sair</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+        ]
+      </Flex>
+
+      <Stack>
+        {
+          tasks.map(item =>
+
+            <Text color='white' key={item.id}> {item.title}</Text>
+          )
+        }
+      </Stack>
 
     </Flex>
   );
 }
 
-export default Hello;
+;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const tasks = await prisma.tasks.findMany()
+
+  const data = tasks.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      isDone: item.isDone,
+      date: new Date(item.createdAt).toISOString()
+    }
+  })
+  return {
+    props: {
+      tasks: data
+    }
+  }
+}
